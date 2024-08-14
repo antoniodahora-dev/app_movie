008886d4253f9a481e0420ca03b236cb49adc6a8
antoniodahora-dev <antonio_dahora@hotmail.com>
2024-08-13 21:59:08 -0300

    put project - tela perfil(1.2) 13/08/2024 - 21:59

--- app/src/main/java/com/a3tecnology/appmovie/presenter/main/bottombar/profile/ProfileFragment.kt
+++ app/src/main/java/com/a3tecnology/appmovie/presenter/main/bottombar/profile/ProfileFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.a3tecnology.appmovie.R
 import com.a3tecnology.appmovie.databinding.FragmentHomeBinding
 import com.a3tecnology.appmovie.databinding.FragmentProfileBinding
+import com.a3tecnology.appmovie.domain.model.MenuProfile
+import com.a3tecnology.appmovie.domain.model.MenuProfileType
+import com.a3tecnology.appmovie.domain.model.MenuProfileType.*
+import com.a3tecnology.appmovie.presenter.main.bottombar.profile.adapter.ProfileMenuAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
+//aula 363
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
@@ -18,6 +24,8 @@ class ProfileFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    private lateinit var profileMenuAdapter: ProfileMenuAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,6 +33,51 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configData()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        profileMenuAdapter = ProfileMenuAdapter(
+            items = MenuProfile.items,
+            context = requireContext(),
+            onClick = { type ->
+                when (type) {
+                    PROFILE -> {}
+                    NOTIFICATION -> {}
+                    DOWNLOAD -> {}
+                    SECURITY -> {}
+                    LANGUAGE -> {}
+                    DARK_MODE -> {}
+                    HELP -> {}
+                    PRIVACY_POLICY -> {}
+                    LOGOUT -> {}
+                }
+            }
+        )
+
+        with(binding.recyclerItem) {
+            setHasFixedSize(true)
+            adapter = profileMenuAdapter
+        }
+
+    }
+
+    private fun configData() {
+        binding.imgProfile.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.ic_profile
+            )
+        )
+
+        binding.txtNameProfile.text = "Antonio da Hora"
+        binding.txtEmailProfile.text = "antonio_dahora@hotmail.com"
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/a3tecnology/appmovie/presenter/main/bottombar/profile/adapter/ProfileMenuAdapter.kt
@@ -0,0 +1,55 @@
+package com.a3tecnology.appmovie.presenter.main.bottombar.profile.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.a3tecnology.appmovie.R
+import com.a3tecnology.appmovie.databinding.ItemUserProfileBinding
+import com.a3tecnology.appmovie.domain.model.MenuProfile
+import com.a3tecnology.appmovie.domain.model.MenuProfileType
+
+// aula 363
+class ProfileMenuAdapter(
+   private val items: List<MenuProfile>,
+    private val context: Context,
+    private val onClick: (MenuProfileType) -> Unit
+
+) : RecyclerView.Adapter<ProfileMenuAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       return MyViewHolder(
+           ItemUserProfileBinding.inflate(
+               LayoutInflater.from(parent.context),
+               parent,
+               false
+           )
+       )
+    }
+
+    override fun getItemCount(): Int {
+      return items.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+       val item = items[position]
+
+        holder.binding.txtItemProfile.apply {
+            text = context.getString(item.text)
+            if (item.type == MenuProfileType.LOGOUT) {
+                setTextColor(ContextCompat.getColor(context, R.color.color_default))
+            }
+        }
+
+        holder.binding.imgItemProfile.setImageDrawable(
+            ContextCompat.getDrawable(context, item.icon)
+        )
+        holder.itemView.setOnClickListener {
+            onClick(item.type)
+        }
+    }
+
+    inner class MyViewHolder(val binding: ItemUserProfileBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
